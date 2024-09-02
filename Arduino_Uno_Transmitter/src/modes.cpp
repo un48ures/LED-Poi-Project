60aaae92cdd6b0ef57adcacd327956568a3bbaba
Markus Geister <megatueftler94@gmail.com>
2024-09-02 14:47:25 +0200

    deleted deprecated and unused midi functions

--- Arduino_Uno_Transmitter/src/modes.cpp
+++ Arduino_Uno_Transmitter/src/modes.cpp
@@ -4,9 +4,6 @@
 #include <modes.h>
 #include <LibPrintf.h>
 
-#define NOTE_ON 144
-#define PITCH_BEND 224
-
 // Poti
 int poti = A0;       // Poti Anschluss A0
 
@@ -28,43 +25,6 @@ byte array0[6][5] = {};
 
 int brightness = 0;
 int brightness_old = 0;
-
-void midi_mode(RF24 *radio, int total_number_receivers, receiver* teensy)
-{
-    // HAUPTFUNKTION:
-    // Das Musik-Programm sendet Töne und deren Pitch-Bend über MIDI/Seriell an den Arduino
-    // 144+Kanal = Note-on Befehl gefolgt von einem Byte für die Tonhöhe -> hier: Instrument 1(Kanal 1) für Poi 1, Tonhöhe 60 für Bild 1
-    // 224+Kanal = Pitch Bend -> hier: Signal für Helligkeit
-    // Serielle Schnittstelle nach neuen Paketen abfragen
-    int data0 = 0;
-    if (Serial.available() > 1)
-    {
-        radio->openWritingPipe(pipe_address);
-        data0 = Serial.read();
-        // Check if first byte is Note On or Pitch Bend
-        if (data0 >= NOTE_ON && data0 < (NOTE_ON + total_number_receivers))
-        {
-            //NOTE ON -> 144 (1st channel); NOTE ON -> 145 (2nd channel)...
-            array0[data0 - NOTE_ON][0] = data0;
-            array0[data0 - NOTE_ON][1] = Serial.read();
-            
-            send_data(&teensy[data0 - NOTE_ON], array0[data0 - NOTE_ON], sizeof(array0[data0 - NOTE_ON]), pipe_address, radio, 1, 1);
-            
-            //LED RED blink when data is send
-            digitalWrite(LED_RED, HIGH);
-            digitalWrite(LED_RED, LOW);
-        }
-        else if (data0 >= PITCH_BEND && data0 < (PITCH_BEND + total_number_receivers))
-        {
-            //PITCH BEND -> 224 (1st channel); PITCH BEND -> 225 (2nd channel)...
-            array0[data0 - PITCH_BEND][2] = data0;
-            array0[data0 - PITCH_BEND][3] = Serial.read();
-            array0[data0 - PITCH_BEND][4] = Serial.read();
-            radio->setChannel(CHs[data0 - PITCH_BEND]);
-            radio->write(&array0[data0 - PITCH_BEND], sizeof(array0[data0 - PITCH_BEND]));
-        }
-    }
-}
 
 void video_light_mode_HW(RF24* radio, receiver* teensy)
 {
